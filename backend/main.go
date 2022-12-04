63d1782d5837c0731d77e4a632782622d5ace073
oovk <vaibhavrkhandekar@gmail.com>
2022-12-04 16:09:29 +0100

    added backend code

---
+++ backend/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/oovk/interact/pkg/websocket"
+	"golang.org/x/net/websocket"
+)
+
+func serveWS(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("websocket endpoint reached")
+
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%V\n", err)
+	}
+	//whenever someone hits the ws endpoint, it opens up a new client which is connected using pool
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+	//after the connection we write the content from client to Register channel of a pool
+	pool.Register <- client
+	client.Read() //read here is a struct method
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool() //function from websocket package
+	go pool.Start()             //starting pool function as a concurrent process
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) { //websocket endpoint
+		serveWS(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Full stack chat application using GoLang and React")
+	setupRoutes()
+	http.ListenAndServe(":9000", nil)
+}
---
+++ backend/pkg/websocket/client.go
@@ -0,0 +1,40 @@
+package websocket
+
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	ID   string
+	Conn *websocket.Conn
+	Pool *Pool
+	mu   sync.Mutex
+}
+
+type Message struct {
+	Type int    `json:"type"`
+	Body string `jsonL:"body"`
+}
+
+// unregister and close the client logic
+func (c *Client) Read() {
+	defer func() {
+		c.Pool.Unregister <- c
+		c.Conn.Close()
+	}() //this is anonymous function which calls itself at the end using defer
+
+	for {
+		messageType, p, err := c.Conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		message := Message{Type: messageType, Body: string(p)}
+		c.Pool.Broadcast <- message
+		fmt.Printf("Message received:%+V\n", message)
+	}
+}
